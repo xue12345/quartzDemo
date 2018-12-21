5328a94bde6d50f1f5549d7329179a02acb57932
wangxue <936878487@qq.com>
2018-12-21 09:47:34 +0800

    定时任务第一版

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@MapperScan("com.example.demo.dao")
+@SpringBootApplication
+@ComponentScan("com.example.demo.**,com.guanyun.wangxue.core")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ demo/src/main/java/com/example/demo/dao/JobAndTriggerMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.entity.JobAndTrigger;
+
+/**
+ * @author Administrator
+ */
+public interface JobAndTriggerMapper {
+	/**
+	 * @return
+	 */
+	public List<JobAndTrigger> getJobAndTriggerDetails();
+}
